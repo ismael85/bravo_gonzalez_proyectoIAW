b5e5c503be01241d3dbdafcb6116864029f23787
ismael <ismaelbg@hotmail.es>
2017-01-26 17:36:16 +0100

    Segundo envio

--- proyecto/index.php
+++ proyecto/index.php
@@ -5,11 +5,31 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1>Bienvenido a la librería online Bravo</h1>
+          <h2><b>Bienvenido a la librería online Bravo</b></h2>
         <p>En este lugar podrá encontrar multitud de libros que puede comprar. Continuamente añadimos novedades. Visítenos con frecuencia para sus compras.</p>
-        
+        <h2><b>NOVEDADES</b></h2>
+          <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+       </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
       </div>
     </div>
+
+      
+   
 
 <?php
     include ('pie/pie.php');
--- proyecto/registre.php
+++ proyecto/registre.php
@@ -4,9 +4,104 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1>Bienvenido a la librería online Bravo</h1>
-        <p>En este lugar podrá encontrar multitud de libros que puede comprar. Continuamente añadimos novedades. Visítenos con frecuencia para sus compras.</p>
         
+      <!-- PHP STRUCTURE FOR CONDITIONAL HTML -->
+      <!-- FIRST TIME. NO DATA IN THE POST (checking a required form field) -->
+      <!-- So we must show the form -->
+
+
+
+      <?php if (!isset($_POST["dni"])) : ?>
+
+        <?php
+          $dni =$_GET['id'];
+
+        //CREATING THE CONNECTION
+        $connection = new mysqli("localhost", "admin", "12345", "proyecto");
+
+        //TESTING IF THE CONNECTION WAS RIGHT
+        if ($connection->connect_errno) {
+            printf("Connection failed: %s\n", $connection->connect_error);
+            exit();
+        }
+
+        //MAKING A SELECT QUERY
+        /* Consultas de selección que devuelven un conjunto de resultados */
+          $query="SELECT * FROM usuarios
+            WHERE nom_usu='$dni'";
+
+        if ($result = $connection->query($query)) {
+
+            $obj = $result->fetch_object();
+
+            $codigo =$obj->CodCliente;
+            $name = $obj->Nombre;
+            $apellidos = $obj->Apellidos;
+            $direccion = $obj->Direccion;
+            $telefono = $obj->Telefono;
+
+
+        }
+
+
+
+        ?>
+
+        <form method="post">
+          <fieldset>
+            <legend>Personal Info</legend>
+            <span>DNI:</span><input type="text" name="dni" value="<?php echo $dni; ?>"required><br>
+            <span>Last Name:</span><input type="text" name="lastname" value="<?php echo $apellidos; ?>" required><br>
+            <span>Name:</span><input type="text" name="name" required value="<?php echo $name; ?>"><br>
+            <span>Address:</span><input type="text" name="address" value="<?php echo $direccion; ?>"><br>
+            <span>Phone: </span><input type="text" name="phone" value="<?php echo $telefono; ?>"><br>
+              <span><input type="hidden" name="codigo"  value="<?php echo $codigo; ?>"
+            <span><input type="submit" value="Editar" >
+          </fieldset>
+        </form>
+
+      <!-- DATA IN $_POST['mail']. Coming from a form submit -->
+      <?php else: ?>
+
+        <?php
+        //CREATING THE CONNECTION
+        $connection = new mysqli("localhost", "tf", "12345", "tf");
+
+        //TESTING IF THE CONNECTION WAS RIGHT
+        if ($connection->connect_errno) {
+            printf("Connection failed: %s\n", $connection->connect_error);
+            exit();
+        }
+
+        //MAKING A UPDATE
+        $codigo=$_POST['codigo'];
+        $nombre=$_POST['name'];
+        $apellidos=$_POST['lastname'];
+        $direccion=$_POST['address'];
+        $telefono=$_POST['phone'];
+        $dni=$_POST['dni'];
+
+
+        $query="Update clientes SET nombre='$nombre',
+        apellidos='$apellidos',
+        direccion='$direccion',
+        telefono='$telefono',
+        dni='$dni'
+        WHERE codcliente='$codigo'";
+
+        $result = $connection->query($query);
+        if (!$result) {
+
+            echo "WRONG QUERY";
+        }
+
+
+        ?>
+
+      <?php endif ?>
+
+  </body>
+</html>
       </div>
     </div>
 
