02612ca8afefd1a247834f45c626decb94063f5b
ismael <ismaelbg@hotmail.es>
2017-01-26 16:06:13 +0100

    commint inicial 1º envio

---
+++ proyecto/vendidos.php
@@ -0,0 +1,15 @@
+<?php
+    include ('cabecera/cabecera.php');
+?>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Bienvenido a la librería online Bravo</h1>
+        <p>En este lugar podrá encontrar multitud de libros que puede comprar. Continuamente añadimos novedades. Visítenos con frecuencia para sus compras.</p>
+        
+      </div>
+    </div>
+
+<?php
+    include ('pie/pie.php');
+?>  
