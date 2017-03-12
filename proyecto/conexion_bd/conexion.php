7327ecdb492f0d194d6a7927ff80c89b5a44af42
ISMAEL <ismaelbg@hotmail.es>
2017-03-12 17:41:46 +0100

    Correcciones

--- proyecto/conexion_bd/conexion.php
+++ proyecto/conexion_bd/conexion.php
@@ -1,7 +1,7 @@
  <?php
  //CREATING THE CONNECTION
                 $connection = new mysqli("localhost", "admin", "12345", "proyecto");
-                $connection->set_charset("uft8");
+               
                 //TESTING IF THE CONNECTION WAS RIGHT
                     if ($connection->connect_errno) {
                         printf("Connection failed: %s\n", $connection->connect_error);
